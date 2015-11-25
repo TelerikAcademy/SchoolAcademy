b7f93dfd67ecf3d1453080fa4aab64e5d4988ed0
evlogihr <evlogihristov@yahoo.com>
2015-11-25 17:59:20 +0200

    Added Live demos

---
+++ 2015-11-Java-OOP/03. Creating-and-using-objects/demos/Startup.java
@@ -0,0 +1,22 @@
+import animals.Dog;
+
+import java.time.*;
+import java.util.ArrayList;
+
+public class Startup {
+  public static void main(String[] args) {
+//    Dog sharo = new Dog("Sharo", 7, "ulichna prevushodna - \"doberman\"");
+//    sharo.bark();
+//    sharo.eat();
+
+//    int[] nums = new int[10];
+//
+//    ArrayList<Integer> arr = new ArrayList<>();
+//    for (int i = 0; i < 100; i++) {
+//      arr.add(i);
+//    }
+
+    Dog myDog = new Dog();
+    System.out.println(new ArrayList().getClass().getName());
+  }
+}
---
+++ 2015-11-Java-OOP/03. Creating-and-using-objects/demos/animals/Dog.java
@@ -0,0 +1,5 @@
+package animals;
+
+public class Dog {
+
+}
